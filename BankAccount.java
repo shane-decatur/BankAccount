e2e647c20556d4659d673a14e0f00a136d73dd19
shane-decatur <sdecatur20@stuy.edu>
2020-10-27 15:41:24 -0400

    made a constructor, values, and toString() method

---
+++ BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount{
+
+  private double balance = 0.0;
+  public int accountID;
+  public String password;
+
+  public BankAccount(int ID, String word){
+    accountID = ID;
+    password = word;
+  }
+
+  public String toString(){
+    return accountID+"\t"+balance;
+  }
+}
